039f85cf80b42ad1c588672ef3bac36162b95349
TJ <taidjerobinson@gmail.com>
2021-04-27 12:53:20 -0700

    CHAPTER1

---
+++ gettimeout.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import socket
+
+def test_socket_timeout():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Default socket timeout: {}".format(s.gettimeout()))
+    s.settimeout(100)
+    print("Current socket timeout: {}".format(s.gettimeout()))
+
+if __name__ == '__main__':
+    test_socket_timeout()
