108d647851f270d5d2b570313c661e62b9eefbf2
tjbronx92 <39333780+tjbronx92@users.noreply.github.com>
2021-04-28 19:25:19 -0700

    Update gettimeout.py

--- gettimeout.py
+++ gettimeout.py
@@ -3,8 +3,10 @@
 import socket
 
 def test_socket_timeout():
+    #Create socket(IPv4,TCP)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("Default socket timeout: {}".format(s.gettimeout()))
+    #timeout on blocking socket operation in seconds
     s.settimeout(100)
     print("Current socket timeout: {}".format(s.gettimeout()))
 
