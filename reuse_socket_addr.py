679b1869bc2401c9ab57d8cc12a06f19b32d0088
tjbronx92 <39333780+tjbronx92@users.noreply.github.com>
2021-05-06 17:51:56 -0700

    Update reuse_socket_addr.py

--- reuse_socket_addr.py
+++ reuse_socket_addr.py
@@ -1,6 +1,6 @@
 #!/usr/bin/venv python3
 
-import SOL_SOCKET
+import SOCKET
 import sys
 
 #getsockopt() and setsockopt() manipulate options for the socket referred to by the file descriptor sockfd.  Options may exist at multiple protocol levels; they are always present at the uppermost socket level.
